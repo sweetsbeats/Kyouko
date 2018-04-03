fbd60387256fdb0dadb8f90ea31a28ae17ddb61f
sweetsbeats <the.patton@yahoo.ca>
2018-04-02 20:41:42 -0700

    Added Logging; Fixed commands working in DMs

--- kyouko.js
+++ kyouko.js
@@ -11,39 +11,57 @@ client.on('ready', () => {
 });
 //			checks every new message
 client.on('message', msg => {
-  if (msg.content.charAt(0) === '!') {		//checks if new message contains command char
+	
+	if (msg.channel.type === 'dm') {
+	//Don't take commands from DMs, could leak or mess with lobby info	
+	} else {
+	
+		if (msg.content.charAt(0) === '!') {		//checks if new message contains command char
 	  
-	 var m = msg.content.slice(1);			//cut off command char					
+		var m = msg.content.slice(1);			//cut off command char					
 //			Starts checking for implemented commands
 //ASS	
-	  if ( m === "ass") {
-		 msg.reply('gimmi');
-		 
+			if ( m === "ass") {
+				msg.reply('gimmi');	 
+				logEvent('!Ass', msg);
 //LOBBY
-	 } else if (m === 'lobby') {
-		 var ch = msg.channel;
-		 msg.channel.send('\n Lobby Name: ..... \nPassword: ...');
+			} else if (m === 'lobby') {
+				var ch = msg.channel;
+				msg.channel.send('\n Lobby Name: ..... \nPassword: ...');
+
+				logEvent('Lobby info grab', msg);
 		 
 //D## 
-	} else if (m.charAt(0) === 'd') {
-		 var m = m.slice(1);
-		 var n = Number(m);
-		 var ret = Math.floor((Math.random() * n) + 1);
+			} else if (m.charAt(0) === 'd') {
+				var m = m.slice(1);
+				var n = Number(m);
+				var ret = Math.floor((Math.random() * n) + 1);
 		
-		 var ch = msg.channel;
-		 ch.send(String(ret));
+				var ch = msg.channel;
+				ch.send(String(ret));
+				logEvent('Dice roll', msg);
 //HELP
-	 } else if ( m === 'help') {
-		 msg.author.sendMessage(String(helpmsg));
-	 }
-  }
+			} else if ( m === 'help') {
+				msg.author.send(String(helpmsg));
+				logEvent('Help asked for', msg);
+			}
+		}
+	}
 });
 client.login(token);		//Passes API token to Discord
+
+//Takes simple description of event and the message to give a detailed description of bot usage
+function logEvent(eventDescriptor, message) {
+	console.log(
+	eventDescriptor + ' from user "' + message.author.username + 
+	'" In server: "' + message.guild.name + 
+	'" In channel: "' + message.channel.name + '"');
+}
 
 var helpmsg = ` 
 		 List of Commands:
 		 
-		 help: 'No shit theres a help function you numb-nutter'
+		 help:  'No shit theres a help function you numb-nutter'
 		 lobby: 'Gets current OJ lobby info'
 		 d##:   'Dice rolls of any sided die you please'
 		 ass:   'Gives kyouko a craving for your ass'
